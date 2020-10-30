0df921d3c5d4a7ec218adf998915ec94bcd2bbb7
Stefanos Larkou <S.Larkou2@newcastle.ac.uk>
2020-10-30 14:53:33 +0000

    Update hello_world.py and add requirements.txt

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,6 @@
 import sys
 import platform
+from direct.showbase.ShowBase import ShowBase
 
 print("hello")
 
@@ -7,3 +8,19 @@ print("hello")
 print( 1, sys.version )
 print( 2, platform.python_implementation())
 print( 3, sys.executable)
+
+class MyApp(ShowBase):
+
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        # Load the environment model.
+        self.scene = self.loader.loadModel("models/environment")
+        # Reparent the model to render.
+        self.scene.reparentTo(self.render)
+        # Apply scale and position transforms on the model.
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
+
+app = MyApp()
+app.run()
