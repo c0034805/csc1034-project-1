54664f166d90c745216b10bcb8b3c2fe4a518823
Stefanos Larkou <S.Larkou2@newcastle.ac.uk>
2020-10-30 16:04:30 +0000

    Add arguments and sound, update requirements.txt and README.md

--- walking_panda/cli.py
+++ walking_panda/cli.py
@@ -1,10 +1,17 @@
 from . import panda
 import argparse
 
+
 def cli():
     parser = argparse.ArgumentParser(prog="walking_panda")
     parser.add_argument("--no-rotate", help="Suppress Rotation",
+                        action="store_true")
+    parser.add_argument("--reverse", help="Move Camera In Reverse",
                         action = "store_true")
+    parser.add_argument("--no-walk", help="Suppress Walking",
+                        action="store_true")
+    parser.add_argument("--scale", type=int, help="Scale Panda Size")
+    parser.add_argument("--location", type=int, nargs='+', help="Set Panda's Location")
     args = parser.parse_args()
 
     walking = panda.WalkingPanda(**vars(args))
--- walking_panda/panda.py
+++ walking_panda/panda.py
@@ -2,21 +2,24 @@ from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
 from math import pi, sin, cos
-
+from playsound import playsound
 
 class WalkingPanda(ShowBase):
 
-    def __init__(self, no_rotate=False):
+    def __init__(self, no_rotate=False, reverse=False,
+                 no_walk=False, scale=1, location=[-8, 42]):
         ShowBase.__init__(self)
-
+        playsound("forest.mp3", False)
         # Load the environment model.
         self.scene = self.loader.loadModel("models/environment")
         # Reparent the model to render.
         self.scene.reparentTo(self.render)
         # Apply scale and position transforms on the model.
         self.scene.setScale(0.25, 0.25, 0.25)
-        self.scene.setPos(-8, 42, 0)
-
+        if location:
+            self.scene.setPos(location[0], location[1], 0)
+        else:
+            self.scene.setPos(-8, 42, 0)
         # Add the spinCameraTask procedure to the task manager.
         self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
 
@@ -24,9 +27,14 @@ class WalkingPanda(ShowBase):
         self.pandaActor = Actor("models/panda-model",
                                 {"walk": "models/panda-walk4"})
         self.pandaActor.setScale(0.005, 0.005, 0.005)
+        if scale:
+            self.pandaActor.setScale(0.005 * scale, 0.005 * scale, 0.005 * scale)
         self.pandaActor.reparentTo(self.render)
         # Loop its animation.
-        self.pandaActor.loop("walk")
+        if not no_walk:
+            self.pandaActor.loop("walk")
+        self.no_rotate = no_rotate
+        self.reverse = reverse
         self.no_rotate = no_rotate
 
     # Define a procedure to move the camera.
@@ -34,7 +42,10 @@ class WalkingPanda(ShowBase):
         if self.no_rotate:
             angleDegrees = 0
         else:
-            angleDegrees = task.time * 6.0
+            if self.reverse:
+                angleDegrees = task.time * -6.0
+            else:
+                angleDegrees = task.time * 6.0
         angleRadians = angleDegrees * (pi / 180.0)
         self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
         self.camera.setHpr(angleDegrees, 0, 0)
